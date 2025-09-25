ec318b12eba4580719ca01f90567d5d94533a7dc
Kajal <226605008+kajall12@users.noreply.github.com>
2025-09-25 12:05:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+     
+
+const display = document.querySelector('.value');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach( (item) => {
+    item.onclick = () => {
+        try {
+        if(item.dataset.button === 'AC') {
+            display.value = '';
+        } else if(item.dataset.button === 'DEL') {
+            let string = display.value;
+            display.value = string.substr(0, string.length-1);
+        } else if(item.dataset.button === '=') {
+            if(display.value !== ''){
+            display.value = eval(display.value);
+        } }
+    else {
+            display.value += item.dataset.button;
+        }
+    }
+         catch  (err) {
+            display.value = 'Invalid Entry';
+            setTimeout( () => (display.value = ''), 1000)
+        }
+    
+    }
+})
